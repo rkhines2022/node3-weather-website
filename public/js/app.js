4bf2ee3054977f19169e9f27b649bac8b0e39422
Ray Hines <rkhines@cox.net>
2022-05-03 12:44:49 -0700

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = ''
 
 
-    fetch('http://localhost:3000/weather?search=' + location).then ((response) => {
+    fetch('/weather?search=' + location).then ((response) => {
         response.json().then ((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
